a39d80b307f044404d0c4c0c93f795a21be8d0d4
loket13 <160454854+loket13@users.noreply.github.com>
2024-09-09 09:47:10 +0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -5,23 +5,45 @@ window.Telegram.WebApp.ready();
 const tg = window.Telegram.WebApp;
 
 // Set the main button color, text, and visibility
-tg.MainButton.text = "Press Me!";
-tg.MainButton.color = "#FF5722";
-tg.MainButton.show();
+tg.BottomButton.text = "Press Me!";
+tg.BottomButton.color = "#FF5722";
+tg.BottomButton.show();
 
 // Setup close button click action to close the WebApp
 document.getElementById("closeBtn").addEventListener("click", function (){
   tg.close();
 });
 
+
+async function sett(k,v){
+  let res = await tg.CloudStorage.setItem(k,v);
+  return res;
+};
+async function gett(k){
+  return tg.CloudStorage.getItem(k);
+};
+document.getElementById("setDataBtn").addEventListener("click", async function(){
+  let key = document.getElementById('setKeyInput').value;
+  let val = document.getElementById('setValInput').value;
+  let res = await sett(key, val);
+  console.log(res);
+  document.getElementById('resSetBox').value = res;
+});
+document.getElementById("getDataBtn").addEventListener("click", async function(){
+  let key = document.getElementById('getKeyInput').value;
+  let val = await gett(key);
+  console.log(val);
+  document.getElementById('resGetBox').value = val;
+});
+
 // Handle the main button being pressed
-tg.MainButton.onClick(function (){
+tg.BottomButton.onClick(function (){
   alert('Main button was clicked!');
   // Send data back to the Telegram bot
   tg.sendData('Main button was clicked!');
   
   // You can hide the main button after the first click, if needed
-  tg.MainButton.hide();
+  tg.BottomButton.hide();
 });
 
 // Make the WebApp theme match the Telegram client theme
